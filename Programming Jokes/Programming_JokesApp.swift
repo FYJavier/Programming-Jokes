51540bb539be013cc527320633657a0c839757fe
Freya Javier <thesleepyunicorn63@gmail.com>
2022-07-26 10:50:58 -0500

    Initial Commit

---
+++ Programming Jokes/Programming_JokesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Programming_JokesApp.swift
+//  Programming Jokes
+//
+//  Created by Yuna on 7/26/22.
+//
+
+import SwiftUI
+
+@main
+struct Programming_JokesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
